87d5ed01686f12f92fd2105158e106f53cfda794
Clint Riley <rileyclint@gmail.com>
2016-08-03 15:08:25 -0700

    More docstrings

--- main_holdem_odds.py
+++ main_holdem_odds.py
@@ -1,38 +1,86 @@
+"""Main entry point for Hold 'em odds evaluator.
+
+Sample invocations:
+Full interactive run: $ python main_holdem_odds.py
+Specific hands: $ python main_holdem_odds.py --hands=AsAd,KsKd
+Specific hands without any interaction for dead cards and board cards:
+    $ python main_holdem_odds.py --hands=AsAd,KsKd,2c3c --nointeraction
+"""
 import argparse
 
 import monte_carlo_runner
 import poker_hand
 
 
-def get_player_hands(hands=None):
+def get_player_hands(hands=''):
+    """Determine which hands to simulate.
+
+    Note that if we have an empty input, we can't proceed, so we forcibly
+    interact with the user in that case.
+
+    Args:
+        hands: str, representation of which hands to simulate.  In the form:
+            "AsAh,KsKd".
+    Returns:
+        list of HoldemHand, the hands to simulate.
+    """
     if not hands:
-        print 'Please input comma separated hold em hands.  For example, ahad,kskd'
+        print ('Please input comma separated hold em hands.  '
+               'For example, ahad,kskd')
         hands = raw_input()
     return poker_hand.parse_hands_into_holdem_hands(hands)
 
 
-def get_board_cards(board_cards=None):
-    if not board_cards:
-        print 'Please input the board cards, separated by spaces.  For example, "Ah As"'
+def get_board_cards(board_cards='', interaction=True):
+    """Determine which cards to place on the board initially.
+
+    Args:
+        board_cards: str, which cards should be placed on the board in every
+            iteration.  For example, "As,Ah,Ad,Ac".
+        interaction: bool, whether or not we should prompt the user for a value
+            if the input is empty.
+
+    Returns:
+        list of Card, cards to place on the board each iteration.
+    """
+    if not board_cards and interaction:
+        print ('Please input the board cards, separated by spaces.  '
+               'For example, "Ah As"')
         board_cards = raw_input()
     return poker_hand.parse_string_into_cards(board_cards)
 
 
-def get_dead_cards(dead_cards=None):
-    if not dead_cards:
-        print 'Please input any dead cards, separated by spaces.  For example, "Ah As"'
+def get_dead_cards(dead_cards='', interaction=True):
+    """Find which cards should be excluded from being selected in the sim.
+
+    Args:
+        dead_cards: str, which cards should be excluded from consideration in
+            this simulation.
+        interaction: bool, whether or not we should prompt the user for a value
+            if the input is empty.
+
+    Returns:
+        list of Card, cards that should be excluded.
+    """
+    if not dead_cards and interaction:
+        print ('Please input any dead cards, separated by spaces.  '
+               'For example, "Ah As"')
         dead_cards = raw_input()
     return poker_hand.parse_string_into_cards(dead_cards)
 
 
-def main(args):
-    player_he_hands = get_player_hands(hands=args.hands)
-    board_cards = get_board_cards(board_cards=args.board_cards)
-    dead_cards = get_dead_cards(dead_cards=args.dead_cards)
+def main(parsed_args):
+    """Run the main program."""
+    player_he_hands = get_player_hands(hands=parsed_args.hands)
+    board_cards = get_board_cards(
+        board_cards=parsed_args.board_cards,
+        interaction=parsed_args.interaction)
+    dead_cards = get_dead_cards(
+        dead_cards=parsed_args.dead_cards, interaction=parsed_args.interaction)
 
     mc_runner = monte_carlo_runner.MonteCarloRunner(
         player_he_hands, board_cards=board_cards, dead_cards=dead_cards,
-        iterations=args.num_iterations)
+        iterations=parsed_args.num_iterations)
     mc_runner.run_all_iterations()
 
 
@@ -57,7 +105,12 @@ def _build_argparse():
         help=('Dead cards.  These will be excluded from consideration in '
               'the hands.'),
         type=str, default='')
-
+    parser.add_argument(
+        '--nointeraction',
+        help='Disable interactively asking for cards.',
+        action='store_false',
+        dest='interaction')
+    parser.set_defaults(interaction=True)
     return parser.parse_args()
 
 
